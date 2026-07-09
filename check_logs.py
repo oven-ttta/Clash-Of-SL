f1ff1e85fa8ea8b5f8f71d76511a3343abc25b94
oven-ttta <sitthisak2007.1@gmail.com>
2026-07-09 23:21:17 +0700

    Enable logging

--- check_logs.py
+++ check_logs.py
@@ -1,7 +1,7 @@
-import urllib.request
-try:
-    response = urllib.request.urlopen('https://owenofsl.ovenx.shop/login')
-    print("Status:", response.status)
-    print(response.read().decode('utf-8'))
-except Exception as e:
-    print("Error:", e)
+import paramiko
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('103.6.168.76', username='root', password='oW4i%8S;$/5Tnsz0EL(7uKa75ujz(QnkpkV%9`x4JA]0Tm1~^z')
+
+stdin, stdout, stderr = ssh.exec_command('systemctl status mysql')
+print(stdout.read().decode('utf-8', errors='replace').encode('ascii', 'ignore').decode('ascii'))
